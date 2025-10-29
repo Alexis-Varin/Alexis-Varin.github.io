b560fed21f0f2ccc71c3a7daf6114c10233593a1
Alexis-Varin <149001252+Alexis-Varin@users.noreply.github.com>
2025-10-29 14:23:26 +0000

    Deploying to gh-pages from @ Alexis-Varin/Alexis-Varin.github.io@32be96fe6474e3c20ffa28472a8f8520077941ea 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -24,7 +24,7 @@ ninja.data = [{
             window.location.href = "/code/";
           },
         },{id: "nav-publications",
-          title: "Publications",
+          title: "publications",
           description: "",
           section: "Navigation",
           handler: () => {
