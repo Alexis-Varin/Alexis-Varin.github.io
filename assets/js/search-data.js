4359b365c88368047ee33c6dd5afdcd5fe728b37
Alexis-Varin <149001252+Alexis-Varin@users.noreply.github.com>
2025-10-30 12:16:05 +0000

    Deploying to gh-pages from @ Alexis-Varin/Alexis-Varin.github.io@ee0d607ca559ae88596aec50ef968f0580e1980a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -47,6 +47,11 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/sc_alignment.html";
+            },},{id: "projects-alignment",
+          title: 'Alignment',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/sc_pseudotime.html";
             },},{
       id: 'light-theme',
       title: 'Change theme to light',
